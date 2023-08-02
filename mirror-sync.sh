2c5b3d5daea0d814633bb048e493ae3f1fc853fb
LockonStratos <lockon.stratos@dreamisty.com>
2023-08-03 00:05:51 +0800

    Refine code

--- mirror-sync.sh
+++ mirror-sync.sh
@@ -56,10 +56,12 @@ repo-set-remote-repo() {
   DRY_RUN=${5}
   cd "$REPO_LOCAL_PATH"
   # check if remote repo has been configured already
-  echo -e "${INDENT} Add remote repository ==> ${GREEN}$MIRROR_REMOTE_REPO_NAME${NC}	${BLUE}$MIRROR_REMOTE_REPO_URL${NC}"
   if ! git remote | grep -q "^$MIRROR_REMOTE_REPO_NAME$"; then
+    echo -e "${INDENT} Add remote repository ==> ${GREEN}$MIRROR_REMOTE_REPO_NAME${NC}	${BLUE}$MIRROR_REMOTE_REPO_URL${NC}"
     [[ $DRY_RUN == true ]] && echo "git remote add $MIRROR_REMOTE_REPO_NAME $MIRROR_REMOTE_REPO_URL" && return 0
     git remote add "$MIRROR_REMOTE_REPO_NAME" "$MIRROR_REMOTE_REPO_URL"
+  else
+    echo -e "${INDENT} Remote repository configured ==> ${GREEN}$MIRROR_REMOTE_REPO_NAME${NC}	${BLUE}$MIRROR_REMOTE_REPO_URL${NC}"
   fi
 }
 
